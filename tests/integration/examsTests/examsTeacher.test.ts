25b89974731c272c03395a4df9ffba43bf07f7b0
liggia <liggia.cruz@gmail.com>
2021-12-17 18:21:42 -0300

    fix: test get /exams/teacher/:teacherId/category/:categoryId

--- tests/integration/examsTests/examsTeacher.test.ts
+++ tests/integration/examsTests/examsTeacher.test.ts
@@ -1,45 +1,52 @@
-// import supertest from "supertest";
-// import app, { init } from '../../../src/app';
-// import { getConnection, getRepository } from "typeorm";
-// import { clearDatabase } from "../../utils/database";
-// import { createExam } from "../../factories/examFactory";
-// import Teacher from "../../../src/entities/Teachers";
-// import faker from 'faker';
+import supertest from "supertest";
+import app, { init } from '../../../src/app';
+import { getConnection, getRepository } from "typeorm";
+import { clearDatabase } from "../../utils/database";
+import { createExam } from "../../factories/examFactory";
+import Teacher from "../../../src/entities/Teachers";
+import faker from 'faker';
+import Exam from "../../../src/entities/Exam";
+import Category from "../../../src/entities/Category";
 
-// describe('GET /exams/teacher/:id', () => {
-//   beforeAll(async () => {
-//           await init();
-//           await clearDatabase();
-//   })
-//   afterAll(async () => {
-//     await clearDatabase();
-//     await getConnection().close();
-//   })
+describe('GET /exams/teacher/:teacherId/category/:categoryId', () => {
+  beforeAll(async () => {
+          await init();
+          await clearDatabase();
+  })
+  afterAll(async () => {
+    await clearDatabase();
+    await getConnection().close();
+  })
   
-//   it('Should return 400 when id is not valid', async () => {
-//     const id = "string";
-//     const result = await supertest(app)
-//     .get(`/exams/teacher/${id}`);
-//      expect(result.status).toEqual(400);
-//   })
+  it('Should return 400 when any id is not valid', async () => {
+    const wrongId = "string";
+    const fakeId = faker.datatype.number();
+    const result = await supertest(app)
+    .get(`/exams/teacher/${fakeId}/category/${wrongId}`);
+     expect(result.status).toEqual(400);
+  })
   
-//   it('Should return 404 when id no exists', async () => {
-//     const id = faker.datatype.number();
-//     const result = await supertest(app)
-//     .get(`/exams/teacher/${id}`);
-//     expect(result.status).toEqual(404);
-//   })
+  it('Should return 404 when any id do not exists', async () => {
+    const fakeId = faker.datatype.number();
+    const result = await supertest(app)
+    .get(`/exams/teacher/${fakeId}/category/${fakeId}`);
+    expect(result.status).toEqual(404);
+  })
   
-//   it('Should return exams when id is valid', async () => {
-//     const name = faker.name.findName();
-//     const nameTeacherFake = faker.name.findName();
-//     const link = faker.internet.url();
-//     await createExam(name, link, nameTeacherFake);
+  it('Should return exams when both ids are valid', async () => {
+    const fakeName = faker.name.findName();
+    const fakeLink = faker.internet.url();
+    await createExam(fakeName, fakeLink);
 
-//     const teacher = await getRepository(Teacher).find({name: nameTeacherFake})
-//     const result = await supertest(app)
-//     .get(`/exams/teacher/${teacher[0].id}`);
-//     expect(result.status).toEqual(200);
-//     expect(result.body.length).toBeGreaterThan(0)
-//   })
-// })
+    const teacher = await getRepository(Teacher).find();
+    const category = await getRepository(Category).find();
+    const exam = await getRepository(Exam).find({
+      relations:['teacher','subject', 'category']
+    })
+    console.log(teacher, category)
+    const result = await supertest(app)
+    .get(`/exams/teacher/${teacher[0].id}/category/${category[0].id}`);
+    expect(result.status).toEqual(200);
+    expect(result.body).toMatchObject(exam)
+  })
+})
