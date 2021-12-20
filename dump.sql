e09847871f3c065e2aac0abe2c77a40869fcbcb0
liggia <liggia.cruz@gmail.com>
2021-12-20 17:03:28 -0300

    dump.sql

--- dump.sql
+++ dump.sql
@@ -3,7 +3,6 @@ CREATE TABLE "exams" (
 	"name" TEXT NOT NULL,
 	"link" TEXT NOT NULL,
 	"categories_id" integer NOT NULL,
-	"semesters_id" integer NOT NULL,
 	"teachers_id" integer NOT NULL,
 	"subjects_id" integer NOT NULL,
 	CONSTRAINT "exams_pk" PRIMARY KEY ("id")
@@ -26,6 +25,7 @@ CREATE TABLE "teachers" (
 CREATE TABLE "subjects" (
 	"id" serial NOT NULL,
 	"name" TEXT NOT NULL,
+	"semester_id" bigint NOT NULL,
 	CONSTRAINT "subjects_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
@@ -65,11 +65,11 @@ CREATE TABLE "semesters" (
 
 
 ALTER TABLE "exams" ADD CONSTRAINT "exams_fk0" FOREIGN KEY ("categories_id") REFERENCES "categories"("id");
-ALTER TABLE "exams" ADD CONSTRAINT "exams_fk1" FOREIGN KEY ("semesters_id") REFERENCES "semesters"("id");
-ALTER TABLE "exams" ADD CONSTRAINT "exams_fk2" FOREIGN KEY ("teachers_id") REFERENCES "teachers"("id");
-ALTER TABLE "exams" ADD CONSTRAINT "exams_fk3" FOREIGN KEY ("subjects_id") REFERENCES "subjects"("id");
+ALTER TABLE "exams" ADD CONSTRAINT "exams_fk1" FOREIGN KEY ("teachers_id") REFERENCES "teachers"("id");
+ALTER TABLE "exams" ADD CONSTRAINT "exams_fk2" FOREIGN KEY ("subjects_id") REFERENCES "subjects"("id");
 
 
+ALTER TABLE "subjects" ADD CONSTRAINT "subjects_fk0" FOREIGN KEY ("semester_id") REFERENCES "semesters"("id");
 
 ALTER TABLE "teachers_subjects" ADD CONSTRAINT "teachers_subjects_fk0" FOREIGN KEY ("teachers_id") REFERENCES "teachers"("id");
 ALTER TABLE "teachers_subjects" ADD CONSTRAINT "teachers_subjects_fk1" FOREIGN KEY ("subjects_id") REFERENCES "subjects"("id");
