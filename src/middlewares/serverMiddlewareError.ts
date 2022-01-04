a20f5447dfc913e8ad40cd69d8d1e849048ca0b8
Nivea <nivea@respondeai.com.br>
2022-01-04 14:54:12 -0300

    preparando para o review

--- src/middlewares/serverMiddlewareError.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export default async function (err:Error, req: Request, res: Response, next: NextFunction) {
-    console.log('SERVER ERROR: ', err);
-    return res.sendStatus(500)
-}
